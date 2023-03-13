174e4037272785df21597c8fcd113c32a50ab1a6
Jhennerson Barbosa da Silva <92925449+jhennerson@users.noreply.github.com>
2023-03-12 21:24:06 -0300

    added bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -10,10 +10,15 @@ public class Pawn extends ChessPiece {
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
-boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		Position p = new Position(0, 0);
 		
@@ -74,10 +79,4 @@ boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		return mat;
 	}
-	
-	@Override
-	public String toString() {
-		return "P";
-	}
-
 }
